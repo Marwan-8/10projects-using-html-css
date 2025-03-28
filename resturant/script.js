892e02b347769537fb47d42dbe45f0b5b46e041e
Marwan <marwanahmedd256@gmail.com>
2025-03-28 21:46:17 +0200

    Add files via upload

---
+++ resturant/script.js
@@ -0,0 +1,15 @@
+console.log("");
+console.log("Hello World");
+console.log("My name is Mithun");
+
+// console.log("Hello World");
+// console.log("My name is Mithun");
+
+// console.log("Hello World");
+// console.log("My name is Mithun");
+   
+
+
+
+screen.log("Hello World");
+screen.log("My name is Mithun");
